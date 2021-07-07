19b653a8051252a3c151c391e3fc9a47c18debfe
ipulbelcram <ipulbelcram@gmail.com>
2021-07-07 21:34:29 +0700

    add all backend

--- app/Models/Param.php
+++ app/Models/Param.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Params extends Model
+class Param extends Model
 {
     protected $table = 'params';
     protected $fillable = [
